43e4bedaa0f18e0e938a8eb658d2af2161c3aae2
Ezeking BB <emendoza_02@outlook.com>
2024-05-28 12:14:51 -0600

    v17

---
+++ view/welcome.view.php
@@ -0,0 +1,17 @@
+<?php
+
+use config\Config;
+
+$config = new Config();
+?>
+
+<div class="d-flex align-items-center justify-content-center vh-100">
+	<div class="text-center">
+		<h1 class="display-1 fw-bold">WELCOME</h1>
+		<p class="fs-3"> <span class="text-danger">TO GOAT FRAMEWORK!</span></p>
+		<p class="lead">
+			visit our repository
+		</p>
+		<a href="https://github.com/ezekingcito/GoatFramework.git" class="btn btn-primary">GOAT-FRAMEWORK</a>
+	</div>
+</div>
