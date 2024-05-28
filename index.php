76db759416599c105d119d2ecfa3518d123e5fc9
Ezeking BB <emendoza_02@outlook.com>
2024-05-28 11:37:51 -0600

    v16

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once realpath('./vendor/autoload.php');
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <script src="./controller/goat.controller.js"></script>
         <title>title</title>
     </head>
     <body>
